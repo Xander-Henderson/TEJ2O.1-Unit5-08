69b12301a5daaae6586ea2812b677138e69b0570
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-10-30 13:00:13 -0400

    self driving car

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Alexander
+ * Created on: Oct 2025
+ * This program is a self driveing car
 */
 
-basic.showString('Hello, World!')
+// variable
+let distanceToObjest: number = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// moves the car forward forever
+while (true) {
+    // gets the distance
+    distanceToObjest = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    if (distanceToObjest > 10) {
+        robotbit.StpCarMove(5, 48)
+        basic.pause(100)
+    } else {
+        robotbit.StpCarMove(-10, 48)
+        basic.pause(100)
+    }
+}
